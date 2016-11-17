eecfe74eba5f458b48f4b24502d4c921200b4331
Logert <soundbux@gmail.com>
2016-11-17 14:48:59 +0300

    рефакторинг

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -4,64 +4,18 @@ const stylelist = {
     marginTop:'5px'
 };
 
-export default class Panel extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            panellist: [],
-            text: ''
-        };
-    }
-    render() {
-        var panelclass = 'panel panel-'+this.props.panelstyle;
-        return (
-            <div className={panelclass}>
-                <div className="panel-heading">
-                    <h3 className="panel-title">{this.props.paneltitle}</h3>
-                </div>
-                <div className="panel-body">
-                    <div className="input-group">
-                        <input ref="theinput" type="text" className="form-control" placeholder="Add..." onChange={this.handleChange.bind(this)}/>
-                        <span className="input-group-btn">
-                            <button onClick={this.handleAdd.bind(this)} className="btn btn-default" type="button">Add</button></span>
-                    </div>
-                    <ul style={stylelist} className="list-group">{this.state.panellist.map((number,id) =>
-                        <li key={id} className="list-group-item">{number}</li>)}
-                    </ul>
-                </div>
-            </div>
-        )
-    }
-    handleChange(e) {
-    this.setState({text: e.target.value});
-    }
-
-    handleAdd () {
-    var currentitems = this.state.panellist;
-    if (this.state.text)
-    {
-        currentitems.push(this.state.text);
-        this.setState({panellist: currentitems});
-        this.refs.theinput.value = '';
-        this.setState({text: ''});
-    }
-}
-}
-/*
-
-
-    getInitialState: function () {
+const Panel = React.createClass({
+    getInitialState() {
         return {
             panellist: [],
             text: ''
         }
     },
-    handleChange: function (e) {
-        this.setState({text: e.target.value})
+    handleChange(e) {
+        this.setState({text: e.target.value});
     },
-
-    handleAdd: function () {
-        var currentitems = this.state.panellist;
+    handleAdd () {
+        let currentitems = this.state.panellist;
         if (this.state.text)
         {
             currentitems.push(this.state.text);
@@ -69,10 +23,9 @@ export default class Panel extends React.Component {
             this.refs.theinput.value = '';
             this.setState({text: ''});
         }
-
     },
-    render: function () {
-        var panelclass = 'panel panel-'+this.props.panelstyle;
+    render() {
+        let panelclass = 'panel panel-'+this.props.panelstyle;
         return (
             <div className={panelclass}>
                 <div className="panel-heading">
@@ -81,7 +34,7 @@ export default class Panel extends React.Component {
                 <div className="panel-body">
                     <div className="input-group">
                         <input ref="theinput" type="text" className="form-control" placeholder="Add..." onChange={this.handleChange}/>
-                            <span className="input-group-btn">
+                        <span className="input-group-btn">
                             <button onClick={this.handleAdd} className="btn btn-default" type="button">Add</button></span>
                     </div>
                     <ul style={stylelist} className="list-group">{this.state.panellist.map((number,id) =>
@@ -91,5 +44,7 @@ export default class Panel extends React.Component {
             </div>
         )
     }
+
 });
-*/
+
+export default Panel;
